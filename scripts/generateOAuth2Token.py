78b0233065fa12c2bb99af3ae79f30dd855c67ba
Thomas Weber <twebe@outlook.de>
2025-10-22 18:50:12 +0000

    Refactoring and simplifying of speech service

--- scripts/generateOAuth2Token.py
+++ scripts/generateOAuth2Token.py
@@ -21,7 +21,7 @@ Output:
 from google.oauth2 import service_account
 import google.auth.transport.requests
 import sys
-import os
+from dotenv import set_key
 
 def get_access_token(json_path, scopes):
     credentials = service_account.Credentials.from_service_account_file(
@@ -32,10 +32,6 @@ def get_access_token(json_path, scopes):
     credentials.refresh(auth_req)
     return credentials.token
 
-def write_env_file(token, env_path=".env"):
-    with open(env_path, "w") as f:
-        f.write(f"OAUTH_ACCESS_TOKEN={token}\n")
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python generateOAuth2Token.py <service_account_json_path> <env_file_path>")
@@ -44,5 +40,5 @@ if __name__ == "__main__":
     env_file_path = sys.argv[2]
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
     token = get_access_token(json_file_path, scopes)
-    write_env_file(token, env_file_path)
-    print(f"OAUTH_ACCESS_TOKEN written to {env_file_path}:\n{token}")
+    set_key(env_file_path, "OAUTH_ACCESS_TOKEN", token)
+    print(f"OAUTH_ACCESS_TOKEN updated in {env_file_path}:\n{token}")
