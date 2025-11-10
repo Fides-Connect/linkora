8afa752c0e897440faf5353f0abe225fe02d0419
Thomas Weber <twebe@outlook.de>
2025-11-10 19:24:26 +0000

    Added integration test for AI Assistant

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,8 @@
 
 # Create Python Virtual Environment and install dependencies
 echo "🐍 Installing Python dependencies..."
-python3 -m venv .venv
+python -m venv .venv
+python -m pip install --upgrade pip
 .venv/bin/pip install -r ./ai-assistant/requirements.txt
 .venv/bin/pip install -r ./connectx/requirements.txt
 echo "source /workspaces/Fides/.venv/bin/activate" >> ~/.bashrc
