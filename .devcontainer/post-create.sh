3d77a4fa934a8420e65d492fcbf3d1521f5c49b2
Thomas Weber <twebe@outlook.de>
2025-11-13 17:53:53 +0000

    Fixed some local issues with DevContainer and Python

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,7 @@
 # Create Python Virtual Environment and install dependencies
 echo "🐍 Installing Python dependencies..."
 python -m venv .venv
-python -m pip install --upgrade pip
+.venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r ./ai-assistant/requirements.txt
 .venv/bin/pip install -r ./connectx/requirements.txt
 echo "source /workspaces/Fides/.venv/bin/activate" >> ~/.bashrc
