04a9b351a662a9e52d0b47860192ce827177ffb1
Thomas Weber <twebe@outlook.de>
2025-11-14 18:03:59 +0000

    Another try to fix the github actions workflow

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,8 +4,7 @@
 echo "🐍 Installing Python dependencies..."
 python -m venv .venv
 .venv/bin/pip install --upgrade pip
-.venv/bin/pip install -r ./ai-assistant/requirements.txt
-.venv/bin/pip install -r ./connectx/requirements.txt
+.venv/bin/pip install -r ./ai-assistant/requirements.txt -r ./connectx/requirements.txt
 echo "source /workspaces/Fides/.venv/bin/activate" >> ~/.bashrc
 
 # Run Flutter doctor to check setup
