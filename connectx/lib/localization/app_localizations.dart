444b7f68304de96e8d1798ce5e8c2619567b3b7e
vinh-cao <quangvinh@me.com>
2025-11-20 12:13:44 +0100

    add language and notification

---
+++ connectx/lib/localization/app_localizations.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'messages_de.dart';
+import 'messages_en.dart';
+
+/// App localization manager
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  // Microphone permission messages
+  String get microphonePermissionTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.microphonePermissionTitle;
+    }
+    return MessagesEN.microphonePermissionTitle;
+  }
+
+  String get microphonePermissionMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.microphonePermissionMessage;
+    }
+    return MessagesEN.microphonePermissionMessage;
+  }
+
+  String get microphoneAccessDeniedTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.microphoneAccessDeniedTitle;
+    }
+    return MessagesEN.microphoneAccessDeniedTitle;
+  }
+
+  String get microphoneAccessDeniedMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.microphoneAccessDeniedMessage;
+    }
+    return MessagesEN.microphoneAccessDeniedMessage;
+  }
+
+  // Notification permission messages
+  String get notificationPermissionTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.notificationPermissionTitle;
+    }
+    return MessagesEN.notificationPermissionTitle;
+  }
+
+  String get notificationPermissionMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.notificationPermissionMessage;
+    }
+    return MessagesEN.notificationPermissionMessage;
+  }
+
+  String get notificationAccessDeniedTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.notificationAccessDeniedTitle;
+    }
+    return MessagesEN.notificationAccessDeniedTitle;
+  }
+
+  String get notificationAccessDeniedMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.notificationAccessDeniedMessage;
+    }
+    return MessagesEN.notificationAccessDeniedMessage;
+  }
+
+  // Common button labels
+  String get okButton {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.okButton;
+    }
+    return MessagesEN.okButton;
+  }
+
+  String get cancelButton {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.cancelButton;
+    }
+    return MessagesEN.cancelButton;
+  }
+
+  String get allowButton {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.allowButton;
+    }
+    return MessagesEN.allowButton;
+  }
+
+  String get denyButton {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.denyButton;
+    }
+    return MessagesEN.denyButton;
+  }
+
+  // Start page messages
+  String get welcomeTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.welcomeTitle;
+    }
+    return MessagesEN.welcomeTitle;
+  }
+
+  String get welcomeMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.welcomeMessage;
+    }
+    return MessagesEN.welcomeMessage;
+  }
+
+  String get signInButton {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.signInButton;
+    }
+    return MessagesEN.signInButton;
+  }
+
+  String get selectLanguage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.selectLanguage;
+    }
+    return MessagesEN.selectLanguage;
+  }
+
+  // Main page messages
+  String get tapMicrophoneToStart {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.tapMicrophoneToStart;
+    }
+    return MessagesEN.tapMicrophoneToStart;
+  }
+
+  String get connecting {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connecting;
+    }
+    return MessagesEN.connecting;
+  }
+
+  String get connected {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connected;
+    }
+    return MessagesEN.connected;
+  }
+
+  String get disconnected {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.disconnected;
+    }
+    return MessagesEN.disconnected;
+  }
+
+  String get connectionClosed {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connectionClosed;
+    }
+    return MessagesEN.connectionClosed;
+  }
+
+  String get connectionEstablishedTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connectionEstablishedTitle;
+    }
+    return MessagesEN.connectionEstablishedTitle;
+  }
+
+  String get connectionEstablishedMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connectionEstablishedMessage;
+    }
+    return MessagesEN.connectionEstablishedMessage;
+  }
+
+  String get connectionLostTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connectionLostTitle;
+    }
+    return MessagesEN.connectionLostTitle;
+  }
+
+  String get connectionLostMessage {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.connectionLostMessage;
+    }
+    return MessagesEN.connectionLostMessage;
+  }
+
+  String get errorTitle {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.errorTitle;
+    }
+    return MessagesEN.errorTitle;
+  }
+
+  String get errorOccurred {
+    if (locale.languageCode == 'de') {
+      return MessagesDE.errorOccurred;
+    }
+    return MessagesEN.errorOccurred;
+  }
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'de'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    return AppLocalizations(locale);
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
